372b92497d2b36e5fa493509843b65157cc6b49b
Euseung <yousi7@naver.com>
2022-06-06 23:44:23 +0900

    pages

---
+++ pages/Channel/index.tsx
@@ -0,0 +1,21 @@
+// import Workspace from "@layouts/Workspace";
+import React from "react";
+import { Container, Header } from "./style";
+
+// workspace 의 children 설정
+const Channel = () => {
+    return (
+        <Container>
+            <Header>
+              로그인에 성공하였습니다. /channel/index
+            </Header>
+        </Container>
+    // <Workspace>     
+    //     <div>
+    //         로그인에 성공하였습니다.
+    //     </div>
+    // </Workspace>
+    )
+};
+
+export default Channel;
---
+++ pages/DirectMessage/index.tsx
@@ -0,0 +1,15 @@
+// import Workspace from "@layouts/Workspace";
+import React from "react";
+
+// workspace 의 children 설정
+const DirectMessage = () => {
+    return (
+    // <Workspace>     
+        <div>
+            Direct Message 성공하였습니다.
+        </div>
+    // </Workspace> 
+    )
+};
+
+export default DirectMessage;
