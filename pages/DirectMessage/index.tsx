9dac257c7bba9e27392cc0de8a9230d165ee5295
Euseung <yousi7@naver.com>
2022-06-15 23:59:18 +0900

    DM

--- pages/DirectMessage/index.tsx
+++ pages/DirectMessage/index.tsx
@@ -1,14 +1,35 @@
 // import Workspace from "@layouts/Workspace";
 import React from "react";
+import { Container, Header } from "./style";
+import gravatar from 'gravatar';
+import useSWR from "swr";
+import { IUser } from "@typings/db";
+import fetcher from "@utils/fetcher";
+import { useParams } from "react-router";
+import ChatBox from "@components/ChatBox";
+import ChatList from "@components/ChatList";
 
 // workspace 의 children 설정
 const DirectMessage = () => {
-    return (
-    // <Workspace>     
-        <div>
-            Direct Message 성공하였습니다.
-        </div>
-    // </Workspace> 
+    const {workspace, id } = useParams<{workspace: string; id:string}>();
+    const {data: userData, error, mutate} = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher,{
+        dedupingInterval: 30000      // 캐시의 유지기간 , 2초동안은 1번만 요청이 감  , 서버에 요청 부담을 줄일수 있음
+    });
+    const {data:myData} = useSWR(`/api/users`, fetcher)
+
+    if(!userData || !myData){
+        return null;
+    }
+
+    return (  
+        <Container>
+            <Header>
+                <img src={gravatar.url(userData.email,{s:'24px', d:'retro'})} alt={userData.nickname} />
+                <span> {userData.nickname}</span>
+            </Header>
+            <ChatList />
+            <ChatBox chat="" />
+        </Container>
     )
 };
 
