78611295e54bcfc4507bfd33283eb41f78a27d46
Euseung <yousi7@naver.com>
2022-06-15 23:59:45 +0900

    chat

--- layouts/Workspace/index.tsx
+++ layouts/Workspace/index.tsx
@@ -100,6 +100,9 @@ const Workspace: VFC = () => {     // children 을 안쓰는 컴포넌트는 VFC
     const onClickAddChannel = useCallback(()=>{
         setShowCreateChannelModal(true)
     },[])
+    const onClickInviteWorkspace = useCallback(()=>{
+        setShowInviteWorkspaceModal(true)
+    },[])
 
     // console.log("userData: "+ JSON.stringify(userData))
     if (!userData) {
@@ -149,12 +152,12 @@ const Workspace: VFC = () => {     // children 을 안쓰는 컴포넌트는 VFC
                         <Menu show={showWorkspaceModal} onCloseModal={toggleWorkspaceModal} style={{top:95, left:80}}>
                             <WorkspaceModal >
                                 <h2>Sleact</h2>
-                                {/* <button onClick={onClickInviteWorkspace}>워크스페이스에 사용자 초대</button> */}
+                                <button onClick={onClickInviteWorkspace}>워크스페이스에 사용자 초대</button>
                                 <button onClick={onClickAddChannel}>채널 만들기</button>
                                 <button onClick={onLogout}>로그아웃</button>
                             </WorkspaceModal>
                         </Menu>
-                        {/* <ChannelList /> */}
+                        <ChannelList />
                         <DMList />
                     </MenuScroll>
                 </Channels>
