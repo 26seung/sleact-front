f1f8eeacba15d982d08808a0a5eb6550d8a7a718
Euseung <yousi7@naver.com>
2022-06-19 20:20:13 +0900

    channel

--- pages/Channel/index.tsx
+++ pages/Channel/index.tsx
@@ -2,16 +2,19 @@
 import ChatBox from "@components/ChatBox";
 import ChatList from "@components/ChatList";
 import useInput from "@hooks/useInput";
+import Workspace from "@layouts/Workspace";
+import axios from "axios";
 import React, { useCallback } from "react";
 import { Container, Header } from "./style";
 
 // workspace 의 children 설정
 const Channel = () => {
 
-    const [chat, onChangeChat] = useInput('');
+    const [chat, onChangeChat,setChat] = useInput('');
     const onSubmitForm = useCallback((e)=>{
         e.preventDefault();
-        console.log(e)
+        setChat('');
+        console.log("submit")
     },[])
 
     return (
