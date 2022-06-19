f38d3cbf7a69cc092803bf3dcfaca96929957c92
Euseung <yousi7@naver.com>
2022-06-19 20:20:23 +0900

    dm

--- pages/DirectMessage/index.tsx
+++ pages/DirectMessage/index.tsx
@@ -3,12 +3,13 @@ import React, { useCallback } from "react";
 import { Container, Header } from "./style";
 import gravatar from 'gravatar';
 import useSWR from "swr";
-import { IUser } from "@typings/db";
+import { IDM, IUser } from "@typings/db";
 import fetcher from "@utils/fetcher";
 import { useParams } from "react-router";
 import ChatBox from "@components/ChatBox";
 import ChatList from "@components/ChatList";
 import useInput from "@hooks/useInput";
+import axios from "axios";
 
 // workspace 의 children 설정
 const DirectMessage = () => {
@@ -16,10 +17,25 @@ const DirectMessage = () => {
     const {data: userData, error, mutate} = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher);
     const {data:myData} = useSWR(`/api/users`, fetcher)
 
-    const [chat, onChangeChat] = useInput('');
+    const [chat, onChangeChat, setChat] = useInput('');
+    const {data:chatData, mutate:mutateChat} = useSWR<IDM[]>(
+        `/api/workspaces/${workspace}/dms/${id}/chat?perPage=20&page=1`,
+        fetcher,
+    )
     const onSubmitForm = useCallback((e)=>{
         e.preventDefault();
-    },[])
+
+        if (chat?.trim()){
+            axios.post(`/api/workspaces/${workspace}/dms/${id}/chats`,{
+                content: chat
+            })
+            .then(()=>{
+                mutateChat(e.data,false)
+                setChat('');
+            })
+            .catch(console.error)
+        }
+    },[chat])
 
 
     if(!userData || !myData){
