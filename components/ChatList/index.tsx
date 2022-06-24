677971ada2b31948afaf15281dbe0fad65402825
Euseung <yousi7@naver.com>
2022-06-24 23:56:31 +0900

    chatList

--- components/ChatList/index.tsx
+++ components/ChatList/index.tsx
@@ -1,21 +1,45 @@
-// // import Chat from '@components/Chat';
+import Chat from '@components/Chat';
 import { ChatZone, Section, StickyHeader } from '@components/ChatList/style';
-// import { IDM, IChat } from '@typings/db';
-import React, { useCallback, forwardRef, RefObject, MutableRefObject } from 'react';
-// // import { Scrollbars } from 'react-custom-scrollbars';
+import { Chats } from '@layouts/Workspace/style';
+import { IDM, IChat } from '@typings/db';
+import React, { useCallback, forwardRef, RefObject, MutableRefObject, VFC, useRef } from 'react';
+import { Scrollbars } from 'react-custom-scrollbars';
+
+interface Props{
+  chatSections: {[key:string]: IDM[] };
+}
+const ChatList: VFC<Props>= ({chatSections})=>{
+  const scrollbarRef = useRef(null);
+  const onScroll = useCallback(()=>{
+
+  },[])
+
+  return (
+    <ChatZone>
+      <Scrollbars autoHide ref={scrollbarRef} onScrollFrame={onScroll}>
+        {Object.entries(chatSections).map(([date,chats])=>{
+          return (
+            <Section className={`section-${date}`} key={date} >
+              <StickyHeader>
+                <button>{date}</button>
+              </StickyHeader>
+                {chats.map((chat)=>(
+                  <Chat key={chat.id} data={chat} />
+                ))}
+            </Section>
+          )
+        })}
+      </Scrollbars>
+    </ChatZone>
+  );
+};
 
 // interface Props {
 //   chatSections: { [key: string]: (IDM | IChat)[] };
 //   setSize: (f: (size: number) => number) => Promise<(IDM | IChat)[][] | undefined>;
 //   isReachingEnd: boolean;
 // }
-const ChatList = () => {
-  return(
-    <ChatZone>
-      <Section></Section>
-    </ChatZone>
-  )
-}
+
 // const ChatList = forwardRef<Scrollbars, Props>(({ chatSections, setSize, isReachingEnd }, scrollRef) => {
 //   const onScroll = useCallback(
 //     (values) => {
