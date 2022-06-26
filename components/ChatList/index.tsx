eb1619d94d0f033c70359b0e057eae34bf784629
Euseung <yousi7@naver.com>
2022-06-26 22:10:51 +0900

    chatList

--- components/ChatList/index.tsx
+++ components/ChatList/index.tsx
@@ -7,16 +7,28 @@ import { Scrollbars } from 'react-custom-scrollbars';
 
 interface Props{
   chatSections: {[key:string]: IDM[] };
+  setSize: (f: (size: number) => number) => Promise<IDM[][] | undefined>
+  isReachingEnd: boolean;
 }
-const ChatList: VFC<Props>= ({chatSections})=>{
-  const scrollbarRef = useRef(null);
-  const onScroll = useCallback(()=>{
+const ChatList = forwardRef<Scrollbars, Props>(({chatSections, setSize, isReachingEnd}, scrollRef )=>{
 
-  },[])
+  const onScroll = useCallback((values)=>{
+    if (values.scrollTop === 0 && !isReachingEnd){
+      console.log("가장 위")
+      setSize((prevSize)=>prevSize+1).then(()=>{
+        // 스크롤 위치 유지
+        const current = (scrollRef as MutableRefObject<Scrollbars>)?.current;
+        if (current) {
+          current.scrollTop(current.getScrollHeight() - values.scrollHeight);
+         }
+      })
+    }
+  },[scrollRef, isReachingEnd, setSize])
+//   sticky 라는 기능이 있다.  ( position: sticky ) 를 사용하면 sticky박스가 scroll 박스에서 벗어나지 않는다.
 
   return (
     <ChatZone>
-      <Scrollbars autoHide ref={scrollbarRef} onScrollFrame={onScroll}>
+      <Scrollbars autoHide ref={scrollRef} onScrollFrame={onScroll}>
         {Object.entries(chatSections).map(([date,chats])=>{
           return (
             <Section className={`section-${date}`} key={date} >
@@ -32,7 +44,7 @@ const ChatList: VFC<Props>= ({chatSections})=>{
       </Scrollbars>
     </ChatZone>
   );
-};
+});
 
 // interface Props {
 //   chatSections: { [key: string]: (IDM | IChat)[] };
