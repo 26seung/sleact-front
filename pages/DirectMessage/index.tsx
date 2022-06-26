d3af9209afad2b0b523325b84f1214b355481577
Euseung <yousi7@naver.com>
2022-06-26 22:10:22 +0900

    dm scroll

--- pages/DirectMessage/index.tsx
+++ pages/DirectMessage/index.tsx
@@ -1,8 +1,9 @@
 // import Workspace from "@layouts/Workspace";
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect, useRef } from "react";
 import { Container, Header } from "./style";
 import gravatar from 'gravatar';
 import useSWR from "swr";
+import useSWRInfinite from 'swr/infinite'
 import { IDM, IUser } from "@typings/db";
 import fetcher from "@utils/fetcher";
 import { useParams } from "react-router";
@@ -11,6 +12,7 @@ import ChatList from "@components/ChatList";
 import useInput from "@hooks/useInput";
 import axios from "axios";
 import makeSection from "@utils/makeSection";
+import Scrollbars from "react-custom-scrollbars";
 
 // workspace 의 children 설정
 const DirectMessage = () => {
@@ -19,31 +21,60 @@ const DirectMessage = () => {
     const {data:myData} = useSWR(`/api/users`, fetcher)
 
     const [chat, onChangeChat, setChat] = useInput('');
-    const {data:chatData, mutate:mutateChat} = useSWR<IDM[]>(
-        `/api/workspaces/${workspace}/dms/${id}/chats?perPage=20&page=1`,
+    const {data:chatData, mutate:mutateChat, setSize} = useSWRInfinite<IDM[]>(
+        (index) => `/api/workspaces/${workspace}/dms/${id}/chats?perPage=20&page=${index+1}`,
         fetcher,
     )
+
+    const isEmpty = chatData?.[0]?.length === 0;
+    const isReachingEnd = isEmpty || (chatData && chatData[chatData.length -1]?.length < 20 || false)
+    const scrollbarRef = useRef<Scrollbars>(null);
     const onSubmitForm = useCallback((e)=>{
         e.preventDefault();
 
-        if (chat?.trim()){
+        if (chat?.trim() && chatData){
+
+            const savedChat = chat;
+
+            mutateChat((prevChatData)=>{
+                prevChatData?.[0].unshift({
+                    id: (chatData[0][0]?.id || 0) + 1,
+                    content: savedChat,   
+                    SenderId: myData.id,
+                    Sender: myData,
+                    ReceiverId: userData.id,
+                    Receiver: userData,
+                    createdAt: new Date(),
+                });
+                return prevChatData;
+            },false)
+            .then(()=>{
+                setChat('');
+                scrollbarRef.current?.scrollToBottom();
+            });
             axios.post(`/api/workspaces/${workspace}/dms/${id}/chats`,{
                 content: chat
             })
             .then(()=>{
-                mutateChat(e.data,true)
-                setChat('');
+                mutateChat();
             })
             .catch(console.error)
         }
-    },[chat])
+    },[chat, chatData, myData, userData, workspace, id])
 
+
+    // 로딩 시 스크롤바 제일 아래로
+    useEffect(()=>{
+        if (chatData?.length === 1){
+            scrollbarRef.current?.scrollToBottom()
+        }
+    },[chatData])
 
     if(!userData || !myData){
         return null;
     }
 
-    const chatSections = makeSection(chatData ? [...chatData].reverse() : [])
+    const chatSections = makeSection(chatData ? [...chatData].flat().reverse() : [])
 
     return (  
         <Container>
@@ -51,7 +82,7 @@ const DirectMessage = () => {
                 <img src={gravatar.url(userData.email,{s:'24px', d:'retro'})} alt={userData.nickname} />
                 <span> {userData.nickname}</span>
             </Header>
-            <ChatList chatSections={chatSections}/>
+            <ChatList chatSections={chatSections} ref={scrollbarRef} setSize={setSize} isReachingEnd={isReachingEnd} />
             <ChatBox chat={chat} onChangeChat={onChangeChat} onSubmitForm={onSubmitForm} />
         </Container>
     );
